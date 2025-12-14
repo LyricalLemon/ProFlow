fec4362e07e2e23d9baaa65cfecec0e9c7a695e8
noah <nd00703@surrey.ac.uk>
2025-12-14 17:15:16 +0000

    Added missing dummy test script, flow_diagram image & setup

---
+++ test_script.py
@@ -0,0 +1,14 @@
+
+def add_numbers(a, b):
+    return a + b
+
+def process_data(data):
+    result = add_numbers(data, 10)
+    print(result)
+
+def main():
+    x = 5
+    process_data(x)
+    process_data(20)
+
+main()
